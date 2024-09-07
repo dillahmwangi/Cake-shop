7cb0ff33feedf2ad79bbd64b21c2f6ae4632e027
dillahmwangi <mcharity241@gmail.com>
2024-09-07 21:01:56 +0300

    Included more routes for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Home} from './Pages/Home';
-// import Footer from './Components/Footer'
-// import Cart from './Pages/Cart'
+import Footer from './Components/Footer'
+import Menu from './Pages/Menu'
+import { Navbar } from './Components/Navbar';
+// import {Cart} from './Pages/Cart'
+import Gallery from './Pages/Gallery';
 
 
 
@@ -10,14 +13,16 @@ import {Home} from './Pages/Home';
 function App() {
   return (
     <Router>
-
+      <Navbar/>
      <main>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/menu' element = {<Menu/>}/>
       {/* <Route path='/cart' element = {<Cart/>}/> */}
+      <Route path='/gallery' element = {<Gallery/>}/>
       </Routes>
      </main>
-   {/* <Footer/> */}
+   <Footer/>
     </Router>
   );
 }
