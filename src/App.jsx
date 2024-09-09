95c0f860d8a0ba42773f3b1dce38ad285de7fcae
dillahmwangi <mcharity241@gmail.com>
2024-09-09 15:12:45 +0300

    Rmoved footer and navbar imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Home} from './Pages/Home';
-import Footer from './Components/Footer'
 import Menu from './Pages/Menu'
-import { Navbar } from './Components/Navbar';
 import {Cart} from './Pages/Cart'
 import Gallery from './Pages/Gallery';
 import Preview from './Pages/Preview';
+import Checkout from './Pages/Checkout';
+
 
 
 
@@ -15,17 +15,16 @@ import Preview from './Pages/Preview';
 function App() {
   return (
     <Router>
-      <Navbar/>
-     <main>
+  
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/menu' element = {<Menu/>}/>
       <Route path='/cart' element = {<Cart/>}/>
       <Route path='/gallery' element = {<Gallery/>}/>
       <Route path = '/preview' element ={<Preview/>}/>
+      <Route path = '/checkOut' element ={<Checkout/>}/>
       </Routes>
-     </main>
-   <Footer/>
+
     </Router>
   );
 }
