97a679cfabab7697ac86b71c24bbe450a1e03711
dillahmwangi <mcharity241@gmail.com>
2024-09-09 15:14:57 +0300

    added the preview as a modal to view description

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,39 +1,24 @@
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import cake from '../assets/cake-one.png'
 import cake3 from '../assets/cake3.png'
-import Preview from './Preview';
+import Layout from '../Components/Layout'
+// import Preview from './Preview';
 
 
 const Gallery = () => {
 
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState(null);
-
-  const openModal = (product) => {
-    setSelectedProduct(product);   
-
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  const products = [
-    { name: 'Espresso Elegante', price: '$10', img: cake },
-    { name: 'Mocha Madness', price: '$12', img: cake3 },
-    // Add other products here
-  ];
+  
 
   return (
-    <div className="bg-pink-100">
+    <Layout>
+    <div className="bg-pink-100" >
     <div className="font-[sans-serif] py-4 mx-auto lg:max-w-6xl max-w-lg md:max-w-full ">
     <h2 className="text-4xl font-extrabold text-gray-800 mb-12 text-center">Coffee store</h2>
 
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-    {products.map((product) => (
-      <div key={product.id} className="bg-gray-200 rounded-xl cursor-pointer hover:scale-[1.03] transition-all relative overflow-hidden">
+   
+      <div className="bg-gray-200 rounded-xl cursor-pointer hover:scale-[1.03] transition-all relative overflow-hidden">
 
         <div className="p-6">
           <div
@@ -53,18 +38,18 @@ const Gallery = () => {
         </div>
 
         <div className="text-center bg-gray-100 p-6">
-          <h3 className="text-lg font-bold text-gray-800">{product.name}</h3>
-          <h4 className="text-lg text-gray-800 font-bold mt-6">{product.price}
+          <h3 className="text-lg font-bold text-gray-800">Espresso</h3>
+          <h4 className="text-lg text-gray-800 font-bold mt-6">10
           </h4>
           <button 
-                onClick={() => openModal(product)}
+              
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
         </div>
       </div>
 
-    ))}
+    
 
       <div className="bg-gray-200 rounded-xl cursor-pointer hover:scale-[1.03] transition-all relative overflow-hidden">
         <div className="p-6">
@@ -90,12 +75,14 @@ const Gallery = () => {
           </h4>
 
           <button 
-               onClick={() => openModal(product)}
+             
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
         </div>
       </div>
+
+
 
       <div className="bg-gray-200 rounded-xl cursor-pointer hover:scale-[1.03] transition-all relative overflow-hidden">
 
@@ -117,7 +104,7 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$14
           </h4>
           <button 
-               onClick={() => openModal(product)}
+               
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -145,7 +132,7 @@ const Gallery = () => {
             className="text-gray-400 ml-2 font-medium">$17</strike>
           </h4>
           <button 
-                onClick={() => openModal(product)}
+             
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -172,7 +159,7 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$15 
           </h4>
           <button 
-                onClick={() => openModal(product)}
+               
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -199,7 +186,7 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$14 
           </h4>
           <button 
-               onClick={() => openModal(product)}
+              
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -226,7 +213,7 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$11 
           </h4>
           <button 
-                onClick={() => openModal(product)}
+              
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -253,7 +240,7 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$13 
           </h4>
           <button 
-                onClick={() => openModal(product)}
+               
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -279,7 +266,7 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$10 
           </h4>
           <button 
-               onClick={() => openModal(product)}
+            
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -305,7 +292,7 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$10 
           </h4>
           <button 
-                onClick={() => openModal(product)}
+                
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
@@ -331,18 +318,44 @@ const Gallery = () => {
           <h4 className="text-lg text-gray-800 font-bold mt-6">$10 
           </h4>
           <button 
-                onClick={() => openModal(product)}
+                
                 className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
                 View Description
               </button>
         </div>
+       
+      </div>
+
+      <div className="bg-gray-200 rounded-xl cursor-pointer hover:scale-[1.03] transition-all relative overflow-hidden">
+        <div className="p-6">
+          <div
+            className="bg-gray-100 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16px" className="fill-gray-800 inline-block" viewBox="0 0 64 64">
+              <path
+                d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                data-original="#000000"></path>
+            </svg>
+          </div>
+          <div className="w-2/3 h-[220px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+            <img src={cake} alt="Product 1" className="h-full w-full object-contain" />
+          </div>
+        </div>
+        <div className="text-center bg-gray-100 p-6">
+          <h3 className="text-lg font-bold text-gray-800">Espresso Elegante</h3>
+          <h4 className="text-lg text-gray-800 font-bold mt-6">$10 
+          </h4>
+          <button 
+                
+                className="w-full flex items-center justify-center gap-3 mt-6 px-8 py-3 bg-pink-500 text-base text-white font-semibold rounded-xl">
+                View Description
+              </button>
+        </div>
+       
       </div>
     </div>
     </div>
-
-    {isOpen && <Preview product={selectedProduct} onClose={closeModal} />}
 </div>
-  
+</Layout>
 
   )
 }
