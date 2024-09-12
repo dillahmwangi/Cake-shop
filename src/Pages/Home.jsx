3e359ef9440bfab9cc13195f1b9b32b31dd4d03d
dillahmwangi <mcharity241@gmail.com>
2024-09-12 16:03:19 +0300

    removed the previous carousel wrapper

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -52,109 +52,9 @@ return (
         </div>
       </section>
     </div>
- {/* <div id="default-carousel" className="relative w-full" data-carousel="slide">
-     <!-- Carousel wrapper --> 
-    <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-         <!-- Item 1 --> 
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-      <div className="transition-all duration-1000 ease-in-out transform bg-pink-200 p-4">
-      <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <div className="text-center space-y-4">
-          <h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
-            Discover the Art of Cake Perfection with Every Slice.
-            <span className="text-pink-500"> Savor the Sweet Moments </span>
-          </h1>
-          <p className="text-gray-500 max-w-xl mx-auto leading-relaxed">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. Where
-            Every Cake Tells a Delicious Story
-          </p>
-        </div>
-        <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
-          <Link
-            to="/checkOut"
-            className="px-8 py-3 w-full bg-pink-500 text-white text-center rounded-md shadow-md block sm:w-auto"
-          >
-            Order Now
-          </Link>
-          <Icon
-            icon="line-md:phone-call-twotone-loop"
-            width="32"
-            height="32"
-            style={{ color: "#ec4899" }}
-          />
-          <Link to="0713289622">0713289622</Link>
-        </div>
-      </section>
-    </div>
-        </div>
-       Item 2  
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-           <div className="transition-all duration-1000 ease-in-out transform bg-pink-200 p-4">
-      <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <div className="text-center space-y-4">
-          <h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
-            Discover the Art of Cake Perfection with Every Slice.
-            <span className="text-pink-500"> Savor the Sweet Moments </span>
-          </h1>
-          <p className="text-gray-500 max-w-xl mx-auto leading-relaxed">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. Where
-            Every Cake Tells a Delicious Story
-          </p>
-        </div>
-        <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
-          <Link
-            to="#"
-            className="px-8 py-3 w-full bg-pink-500 text-white text-center rounded-md shadow-md block sm:w-auto"
-          >
-            Order Now
-          </Link>
-          <Icon
-            icon="line-md:phone-call-twotone-loop"
-            width="32"
-            height="32"
-            style={{ color: "#ec4899" }}
-          />
-          <Link to="0713289622">0713289622</Link>
-        </div>
-      </section>
-    </div>
-        </div>
-        <!-- Item 3 --> 
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <div className="transition-all duration-1000 ease-in-out transform bg-pink-200 p-4">
-      <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <div className="text-center space-y-4">
-          <h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
-            Discover the Art of Cake Perfection with Every Slice.
-            <span className="text-pink-500"> Savor the Sweet Moments </span>
-          </h1>
-          <p className="text-gray-500 max-w-xl mx-auto leading-relaxed">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. Where
-            Every Cake Tells a Delicious Story
-          </p>
-        </div>
-        <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
-          <Link
-            to="#"
-            className="px-8 py-3 w-full bg-pink-500 text-white text-center rounded-md shadow-md block sm:w-auto"
-          >
-            Order Now
-          </Link>
-          <Icon
-            icon="line-md:phone-call-twotone-loop"
-            width="32"
-            height="32"
-            style={{ color: "#ec4899" }}
-          />
-          <Link to="0713289622">0713289622</Link>
-        </div>
-      </section>
-    </div>
-        </div>*/}
-             <div className="bg-pink-100 p-4 ">
+
+    
+    <div className="bg-pink-100 p-4 ">
       <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8">
         <div className="text-center space-y-4">
           <h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
@@ -344,9 +244,9 @@ return (
                                 d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
                                 data-original="#000000" />
                         </svg>
-                        <a href="javascript:void(0)" className="text-sm ml-4">
+                        <Link to="#" className="text-sm ml-4">
                             <strong>info@example.com</strong>
-                        </a>
+                        </Link>
                     </li>
                     <li className="flex items-center text-black">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill='currentColor'
@@ -355,15 +255,15 @@ return (
                                 d="M98.339 320.8c47.6 56.9 104.9 101.7 170.3 133.4 24.9 11.8 58.2 25.8 95.3 28.2 2.3.1 4.5.2 6.8.2 24.9 0 44.9-8.6 61.2-26.3.1-.1.3-.3.4-.5 5.8-7 12.4-13.3 19.3-20 4.7-4.5 9.5-9.2 14.1-14 21.3-22.2 21.3-50.4-.2-71.9l-60.1-60.1c-10.2-10.6-22.4-16.2-35.2-16.2-12.8 0-25.1 5.6-35.6 16.1l-35.8 35.8c-3.3-1.9-6.7-3.6-9.9-5.2-4-2-7.7-3.9-11-6-32.6-20.7-62.2-47.7-90.5-82.4-14.3-18.1-23.9-33.3-30.6-48.8 9.4-8.5 18.2-17.4 26.7-26.1 3-3.1 6.1-6.2 9.2-9.3 10.8-10.8 16.6-23.3 16.6-36s-5.7-25.2-16.6-36l-29.8-29.8c-3.5-3.5-6.8-6.9-10.2-10.4-6.6-6.8-13.5-13.8-20.3-20.1-10.3-10.1-22.4-15.4-35.2-15.4-12.7 0-24.9 5.3-35.6 15.5l-37.4 37.4c-13.6 13.6-21.3 30.1-22.9 49.2-1.9 23.9 2.5 49.3 13.9 80 17.5 47.5 43.9 91.6 83.1 138.7zm-72.6-216.6c1.2-13.3 6.3-24.4 15.9-34l37.2-37.2c5.8-5.6 12.2-8.5 18.4-8.5 6.1 0 12.3 2.9 18 8.7 6.7 6.2 13 12.7 19.8 19.6 3.4 3.5 6.9 7 10.4 10.6l29.8 29.8c6.2 6.2 9.4 12.5 9.4 18.7s-3.2 12.5-9.4 18.7c-3.1 3.1-6.2 6.3-9.3 9.4-9.3 9.4-18 18.3-27.6 26.8l-.5.5c-8.3 8.3-7 16.2-5 22.2.1.3.2.5.3.8 7.7 18.5 18.4 36.1 35.1 57.1 30 37 61.6 65.7 96.4 87.8 4.3 2.8 8.9 5 13.2 7.2 4 2 7.7 3.9 11 6 .4.2.7.4 1.1.6 3.3 1.7 6.5 2.5 9.7 2.5 8 0 13.2-5.1 14.9-6.8l37.4-37.4c5.8-5.8 12.1-8.9 18.3-8.9 7.6 0 13.8 4.7 17.7 8.9l60.3 60.2c12 12 11.9 25-.3 37.7-4.2 4.5-8.6 8.8-13.3 13.3-7 6.8-14.3 13.8-20.9 21.7-11.5 12.4-25.2 18.2-42.9 18.2-1.7 0-3.5-.1-5.2-.2-32.8-2.1-63.3-14.9-86.2-25.8-62.2-30.1-116.8-72.8-162.1-127-37.3-44.9-62.4-86.7-79-131.5-10.3-27.5-14.2-49.6-12.6-69.7z"
                                 data-original="#000000"></path>
                         </svg>
-                        <a href="javascript:void(0)" className="text-sm ml-4">
+                        <Link to="#" className="text-sm ml-4">
                             <strong>+158 996 888</strong>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
             <div className="z-10 relative h-full max-md:min-h-[350px]">
                 <iframe src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                    className="left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" frameborder="0"
+                    className="left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" frameborder="0" title='Map'
                     allowfullscreen></iframe>
             </div>
         </div>
