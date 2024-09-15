8074a3847f0a9a374889e88664c249b5f9b2d664
dillahmwangi <mcharity241@gmail.com>
2024-09-15 23:48:47 +0300

    changed tabl padding

--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -5,7 +5,7 @@ const Payment = () => {
   return (
     <Layout>
 
-<div class="bg-pink-100 p-4">
+<div class="bg-pink-100 p-60 justify-center">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
