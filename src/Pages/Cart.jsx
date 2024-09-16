5dbb2f5b054ef5a6dbebcd0563668c305488dfa0
dillahmwangi <mcharity241@gmail.com>
2024-09-16 08:38:46 +0300

    changed background

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -8,7 +8,7 @@ import Layout from '../Components/Layout'
 export const Cart = () => {
   return (
     <Layout>
-    <div className="bg-pink-100">
+    {/* <div className="bg-pink-100">
     <div className="font-sans py-16 items-center mx-auto max-w-screen-xl sm:px-8 md:flex md:space-x-6">
     <div className="font-sans md:max-w-4xl max-md:max-w-xl mx-auto py-4">
             <div className="grid md:grid-cols-3 gap-4">
@@ -197,7 +197,144 @@ export const Cart = () => {
             </div>
         </div>
         </div>
-        </div>
+        </div> */}
+
+<div class="font-sans bg-white max-w-6xl mx-auto p-4">
+      <h2 class="text-3xl font-bold text-gray-800">Your Cart</h2>
+      <div class="overflow-x-auto">
+        <table class="mt-12 w-full border-collapse divide-y">
+
+          <thead class="whitespace-nowrap text-left">
+            <tr>
+              <th class="text-base text-gray-500 font-medium p-2">Description</th>
+              <th class="text-base text-gray-500 font-medium p-2">Size</th>
+              <th class="text-base text-gray-500 font-medium p-2">Quantity</th>
+              <th class="text-base text-gray-500 font-medium p-2">Remove</th>
+              <th class="text-base text-gray-500 font-medium p-2">Price</th>
+            </tr>
+          </thead>
+
+          <tbody class="whitespace-nowrap divide-y">
+            <tr>
+              <td class="px-2 py-4">
+                <div class="flex items-center gap-4 w-max">
+                  <div class="h-32 shrink-0">
+                    <img src='https://readymadeui.com/images/product6.webp' class="w-full h-full object-contain rounded-lg" />
+                  </div>
+                  <div>
+                    <p class="text-base font-bold text-gray-800">Black T-Shirt</p>
+                  </div>
+                </div>
+              </td>
+
+              <td class="px-2 py-4">
+                <span class="bg-transparent border flex items-center justify-center w-11 h-10 rounded-lg text-base">
+                  M
+                </span>
+              </td>
+
+              <td class="px-2 py-4">
+                <div class="flex overflow-hidden border w-max rounded-lg">
+                  <button type="button" class="bg-gray-100 flex items-center justify-center w-11 h-10 font-semibold">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 fill-current" viewBox="0 0 124 124">
+                      <path d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z" data-original="#000000"></path>
+                    </svg>
+                  </button>
+                  <span class="bg-transparent flex items-center justify-center w-11 h-10 font-semibold text-gray-800 text-base">
+                    1
+                  </span>
+                  <button type="button" class="bg-gray-800 text-white flex items-center justify-center w-11 h-10 font-semibold">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 fill-current" viewBox="0 0 42 42">
+                      <path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z" data-original="#000000"></path>
+                    </svg>
+                  </button>
+                </div>
+              </td>
+
+              <td class="px-2 py-4">
+                <button type="button" class="bg-transparent border flex items-center justify-center w-11 h-10 rounded-lg">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-4 fill-red-500 inline" viewBox="0 0 24 24">
+                    <path
+                      d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z"
+                      data-original="#000000"></path>
+                    <path d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </button>
+              </td>
+
+              <td class="px-2 py-4">
+                <h4 class="text-base font-bold text-gray-800">$18.5</h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td class="px-2 py-4">
+                <div class="flex items-center gap-4 w-max">
+                  <div class="h-32 shrink-0">
+                    <img src='https://readymadeui.com/images/product2.webp' class="w-full h-full object-contain rounded-lg" />
+                  </div>
+                  <div>
+                    <p class="text-base font-bold text-gray-800">Gray T-Shirt</p>
+                  </div>
+                </div>
+              </td>
+
+              <td class="px-2 py-4">
+                <span class="bg-transparent border flex items-center justify-center w-11 h-10 rounded-lg text-base">
+                  M
+                </span>
+              </td>
+
+              <td class="px-2 py-4">
+                <div class="flex overflow-hidden border w-max rounded-lg">
+                  <button type="button" class="bg-gray-100 flex items-center justify-center w-11 h-10 font-semibold">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 fill-current" viewBox="0 0 124 124">
+                      <path d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z" data-original="#000000"></path>
+                    </svg>
+                  </button>
+                  <span class="bg-transparent flex items-center justify-center w-11 h-10 font-semibold text-gray-800 text-base">
+                    1
+                  </span>
+                  <button type="button" class="bg-gray-800 text-white flex items-center justify-center w-11 h-10 font-semibold">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 fill-current" viewBox="0 0 42 42">
+                      <path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z" data-original="#000000"></path>
+                    </svg>
+                  </button>
+                </div>
+              </td>
+
+              <td class="px-2 py-4">
+                <button type="button" class="bg-transparent border flex items-center justify-center w-11 h-10 rounded-lg">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-4 fill-red-500 inline cursor-pointer" viewBox="0 0 24 24">
+                    <path
+                      d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z"
+                      data-original="#000000"></path>
+                    <path d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z"
+                      data-original="#000000"></path>
+                  </svg>
+                </button>
+              </td>
+
+              <td class="px-2 py-4">
+                <h4 class="text-base font-bold text-gray-800">$18.5</h4>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+
+      <div class="max-w-xl ml-auto">
+        <ul class="text-gray-800 divide-y">
+          <li class="flex flex-wrap gap-3 text-base py-3">Subtotal <span class="ml-auto font-bold">$37.00</span></li>
+          <li class="flex flex-wrap gap-3 text-base py-3">Shipping <span class="ml-auto font-bold">$4.00</span></li>
+          <li class="flex flex-wrap gap-3 text-base py-3">Tax <span class="ml-auto font-bold">$4.00</span></li>
+          <li class="flex flex-wrap gap-3 text-base py-3 font-bold">Total <span class="ml-auto">$45.00</span></li>
+        </ul>
+        <button type="button" class="mt-6 text-base tracking-wide px-5 py-2.5 w-full bg-gray-800 hover:bg-gray-900 text-white rounded-lg">Make Payment</button>
+      </div>
+    </div>
         </Layout>
   )
 }
